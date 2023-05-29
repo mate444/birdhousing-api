251471552006cf0bf647e384df9a88f4a55350dc
mate444 <mateoeo23@gmail.com>
2023-05-29 14:35:38 +0200

    Added Birdhouse interface

---
+++ src/birdhouse/interfaces/birdhouse.interface.ts
@@ -0,0 +1,11 @@
+export interface BirdhouseInterface {
+  birdhouseId: string;
+  name: string;
+  price: number;
+  description: string;
+  status: string;
+  size: number;
+  colors: string[];
+  pictures: string[];
+  styles: string[];
+}
