d91cc2732952adec8883b2da0dd5f1e0a1229077
mate444 <mateoeo23@gmail.com>
2023-05-30 12:38:18 +0200

    Updated configuration files

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,15 +1,17 @@
 import cors from 'cors';
 import express from 'express';
+import birdhouseRouter from '../birdhouse/controllers/birdhouse.controller';
 
 const createServer = (): express.Application => {
   const app = express();
+  app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cors());
-  app.use(express.json())
+  app.use('/birdhouse', birdhouseRouter);
   app.get('/', (req: express.Request, res: express.Response) => {
     res.send('Welcome!');
   });
   return app;
-}
+};
 
 export { createServer };
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { createServer } from './config/express';
+require('dotenv').config();
 
 const startServer = async (): Promise<void> => {
   const app = await createServer();
   app.listen(process.env.PORT || 3000, () => {
     console.log('listening on http://localhost:3000');
   });
-}
+};
 
 startServer();
