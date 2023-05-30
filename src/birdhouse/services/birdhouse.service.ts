942e073f5ec5edeb95a430ae65cb00f9ad779326
mate444 <mateoeo23@gmail.com>
2023-05-30 18:27:59 +0200

    Adds method "softDelete" to birdhouse service

--- src/birdhouse/services/birdhouse.service.ts
+++ src/birdhouse/services/birdhouse.service.ts
@@ -1,15 +1,15 @@
-import pool from '../../database/pool';
-import { BirdhouseInterface } from '../interfaces/birdhouse.interface';
+import { query } from '../../common/createQuery';
+import { BirdhouseInterface, BirdhouseStatusEnum } from '../interfaces/birdhouse.interface';
 import { v4 as uuid } from 'uuid';
 import fsPromise from 'fs/promises';
 
 export class BirdhouseService {
-  promisePool = pool.promise();
+  SQLQuery = query;
   async create (data: BirdhouseInterface) {
     try {
       const status = 'active';
       const birdhouseId = uuid();
-      await this.promisePool.query(`INSERT INTO birdhouse (birdhouseId,
+      await this.SQLQuery(`INSERT INTO birdhouse (birdhouseId,
         name,
         price,
         description,
@@ -28,17 +28,17 @@ export class BirdhouseService {
       if (data.pictures) {
         for (let i = 0; i < data.pictures.length; i += 1) {
           const file = await fsPromise.readFile(data.pictures[i].path, 'binary');
-          await this.promisePool.query(`INSERT into birdhouse_picture (birdhouseId, picture) VALUES(?, ?)`,
+          await this.SQLQuery(`INSERT into birdhouse_picture (birdhouseId, picture) VALUES(?, ?)`,
             [birdhouseId, file]);
           await fsPromise.unlink(data.pictures[i].path);
         }
       }
       for (let i = 0; i < data.styles.length; i += 1) {
-        await this.promisePool.query(`INSERT into birdhouse_style (birdhouseId, style) VALUES(?, ?)`,
+        await this.SQLQuery(`INSERT into birdhouse_style (birdhouseId, style) VALUES(?, ?)`,
           [birdhouseId, data.styles[i]]);
       }
       for (let i = 0; i < data.colors.length; i += 1) {
-        await this.promisePool.query(`INSERT into birdhouse_color (birdhouseId, color) VALUES(?, ?)`,
+        await this.SQLQuery(`INSERT into birdhouse_color (birdhouseId, color) VALUES(?, ?)`,
           [birdhouseId, data.colors[i]]);
       }
 
@@ -50,7 +50,7 @@ export class BirdhouseService {
 
   async getById (id: string) {
     try {
-      const foundBirdhouse = await this.promisePool.query(`SELECT
+      const foundBirdhouse = await this.SQLQuery(`SELECT
         name, price, description, stock, size, color, picture, style
         FROM birdhouse
         INNER JOIN birdhouse_color ON birdhouse_color.birdhouseId = birdhouse.birdhouseId
@@ -58,6 +58,25 @@ export class BirdhouseService {
         INNER JOIN birdhouse_picture ON birdhouse_picture.birdhouseId = birdhouse.birdhouseId
         WHERE birdhouse.birdhouseId = ?`, [id]);
       return foundBirdhouse[0][0];
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  async softDelete (id: string, status: BirdhouseStatusEnum) {
+    try {
+      const deletedBirdhouseResult = await this.SQLQuery(`UPDATE birdhouse 
+      SET status = ?
+      WHERE birdhouseId = ?`, [status, id]);
+      if (deletedBirdhouseResult.affectedRows === 0) {
+        return false;
+      }
+      if (status === 'inactive') {
+        return 'Deleted';
+      }
+      if (status === 'active') {
+        return "Restored";
+      }
     } catch (err) {
       throw new Error(err);
     }
