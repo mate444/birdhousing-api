8d329759d193d083783c4c5f67ad9877fe3d5013
mate444 <mateoeo23@gmail.com>
2023-05-31 19:55:22 +0200

    Adds User Interface

--- src/user/interfaces/user.interface.ts
+++ src/user/interfaces/user.interface.ts
@@ -7,3 +7,23 @@ export enum UserRoleEnum {
   admin = 'admin',
   client = 'client'
 }
+
+export interface UserPermissionInterface {
+  id: number;
+  permission: string;
+}
+
+export interface UserRoleInterface {
+  id: number;
+  rolename: string;
+  permissions: UserPermissionInterface[];
+}
+
+export interface UserInterface {
+  id: string;
+  name: string;
+  lastname: string;
+  password: string;
+  email: string;
+  roleId: UserRoleInterface[];
+}
