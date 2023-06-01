e972e126a6486b8709a5e76f3a8baa9e7454aca8
mate444 <mateoeo23@gmail.com>
2023-06-01 20:02:19 +0200

    Adds helmet for better security

--- src/config/express.ts
+++ src/config/express.ts
@@ -2,12 +2,14 @@ import cors from 'cors';
 import express from 'express';
 import birdhouseRouter from '../birdhouse/controllers/birdhouse.controller';
 import userRouter from '../user/controllers/user.controller';
+import helmet from 'helmet';
 
 const createServer = (): express.Application => {
   const app = express();
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cors());
+  app.use(helmet());
   app.use('/birdhouse', birdhouseRouter);
   app.use('/user', userRouter);
   app.get('/', (req: express.Request, res: express.Response) => {
