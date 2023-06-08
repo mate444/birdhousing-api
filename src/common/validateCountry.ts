1753caadb312f89cdab94eb5932bddfd242b8679
mate444 <mateoeo23@gmail.com>
2023-06-08 12:40:19 +0200

    Adds validateCountry helper + general config adjustments

---
+++ src/common/validateCountry.ts
@@ -0,0 +1,19 @@
+import { ValidationOptions, ValidationArguments, registerDecorator } from 'class-validator';
+import { isValid, getAlpha2Code } from 'i18n-iso-countries';
+
+export function IsCountry (validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    return registerDecorator({
+      name: 'isFile',
+      target: object.constructor,
+      propertyName,
+      constraints: [],
+      options: validationOptions,
+      validator: {
+        validate (value: any, args: ValidationArguments) {
+          return typeof value === 'string' && isValid(getAlpha2Code(value, 'en'));
+        }
+      }
+    });
+  };
+}
--- src/config/express.ts
+++ src/config/express.ts
@@ -4,14 +4,17 @@ import birdhouseRouter from '../birdhouse/controllers/birdhouse.controller';
 import userRouter from '../user/controllers/user.controller';
 import helmet from 'helmet';
 import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
 
 const createServer = (): express.Application => {
   const app = express();
+  const morganMiddleware = process.env.NODE_ENV === 'production' ? morgan("common") : morgan("dev");
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cors());
   app.use(helmet());
   app.use(cookieParser());
+  app.use(morganMiddleware);
   app.use('/birdhouse', birdhouseRouter);
   app.use('/user', userRouter);
   app.get('/', (req: express.Request, res: express.Response) => {
