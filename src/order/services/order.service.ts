116e3c240a230585e88e0e765264c6ae967c38da
mate444 <mateoeo23@gmail.com>
2023-06-25 21:21:18 +0200

    Adds method "createStripeSession" to order service

--- src/order/services/order.service.ts
+++ src/order/services/order.service.ts
@@ -4,8 +4,13 @@ import { Order } from "../entities/order.entity";
 import { IOrder, OrderStatusEnum } from "../interfaces/order.interface";
 import { User } from "../../user/entities/User.entity";
 import { ILike } from "typeorm";
+import Stripe from "stripe";
 
 export class OrderService {
+  stripe = new Stripe(`${process.env.STRIPE_APIKEY}`, {
+    apiVersion: "2022-11-15"
+  });
+
   entityManager = Manager;
   async create (data: IOrder) {
     try {
@@ -82,6 +87,64 @@ export class OrderService {
       }
       const orders = await this.entityManager.find(Order, findOptions);
       return { data: orders, totalPages };
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  async createStripeSession (data: {
+    price: number,
+    userId: string,
+    birdhouses: {
+      id: string,
+      amount: number
+    }[]
+  }, token: string) {
+    try {
+      const line_items = [];
+      for (const b of data.birdhouses) {
+        const foundBirdhouse = await this.entityManager.findOne(Birdhouse, {
+          where: {
+            birdhouseId: b.id
+          }
+        });
+        if (!foundBirdhouse) return "Not found";
+        if (foundBirdhouse.stock < b.amount) return "Not enough stock";
+        line_items.push({
+          quantity: b.amount,
+          price_data: {
+            currency: 'EUR',
+            product_data: {
+              name: foundBirdhouse.name,
+              images: foundBirdhouse.pictures,
+              description: foundBirdhouse.description
+            },
+            unit_amount: foundBirdhouse.price * 100 // Convert price value to cents
+          }
+        });
+      }
+      const foundUser = await this.entityManager.findOne(User, {
+        where: {
+          id: data.userId
+        }
+      });
+      if (!foundUser) return "Not found";
+      const session = await this.stripe.checkout.sessions.create({
+        success_url: "http://localhost:3001",
+        cancel_url: "http://localhost:3001/notfound",
+        payment_method_types: ['card'],
+        mode: 'payment',
+        line_items,
+        customer_email: foundUser.email,
+        payment_intent_data: {
+          metadata: {
+            token,
+            birdhouses: JSON.stringify(data.birdhouses),
+            price: data.price
+          }
+        }
+      });
+      return session.url;
     } catch (err) {
       throw new Error(err);
     }
