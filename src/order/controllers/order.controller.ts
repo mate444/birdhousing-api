a91c3d86bf5b22da3aa553b5d8af7d9d3869f5cd
mate444 <mateoeo23@gmail.com>
2023-06-25 21:22:34 +0200

    Adds POST /orders/checkout

--- src/order/controllers/order.controller.ts
+++ src/order/controllers/order.controller.ts
@@ -1,7 +1,7 @@
 import { Router, Request, NextFunction, Response } from "express";
 import { validate } from 'class-validator';
 import { OrderService } from "../services/order.service";
-import { isNotLoggedIn } from "../../middleware/auth";
+import { isAdmin, isNotLoggedIn } from "../../middleware/auth";
 import { CreateOrderDto, UpdateOrderStatusDto } from "../dtos/order.dto";
 
 const router = Router();
@@ -29,8 +29,7 @@ router.post('/', isNotLoggedIn, async (req: Request, res: Response, next: NextFu
     res.sendStatus(500);
   }
 });
-
-router.patch('/status', async (req: Request, res: Response, next: NextFunction) => {
+router.patch('/status', isAdmin, async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { status, id } = req.body;
     const orderDto = new UpdateOrderStatusDto();
@@ -50,7 +49,7 @@ router.patch('/status', async (req: Request, res: Response, next: NextFunction) 
   }
 });
 
-router.get('/', async (req: Request, res: Response, next: NextFunction) => {
+router.get('/', isAdmin, async (req: Request, res: Response, next: NextFunction) => {
   try {
     let { page, search } = req.query;
     if (page === 'undefined') page = undefined;
@@ -59,6 +58,30 @@ router.get('/', async (req: Request, res: Response, next: NextFunction) => {
     const orderService = new OrderService();
     const orders = await orderService.getAll(search, parseInt(`${page}`));
     res.send(orders);
+  } catch (err) {
+    console.log(err.message, err.stack);
+    res.sendStatus(500);
+  }
+});
+
+router.post('/checkout', async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { birdhouses, price, userId } = req.body;
+    const orderDto = new CreateOrderDto();
+    orderDto.birdhouses = birdhouses;
+    orderDto.price = price;
+    orderDto.userId = userId;
+    const errors = await validate(orderDto);
+    if (errors.length) {
+      return res.status(400).send(errors);
+    }
+    const orderService = new OrderService();
+    const sessionUrl = await orderService.createStripeSession(req.body, req.cookies.auth);
+    if (sessionUrl === "Not found") return res.status(404).send(sessionUrl);
+    if (sessionUrl === "Not enough stock") {
+      return res.status(400).send("There is no more stock available for this birdhouse");
+    }
+    res.send(sessionUrl);
   } catch (err) {
     console.log(err.message, err.stack);
     res.sendStatus(500);
