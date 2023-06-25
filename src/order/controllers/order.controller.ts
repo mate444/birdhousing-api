ac850bb0151ca6588429585577ac9e76d89193a4
mate444 <mateoeo23@gmail.com>
2023-06-25 21:25:56 +0200

    Modifies POST /orders for Stripe webhook integration

--- src/order/controllers/order.controller.ts
+++ src/order/controllers/order.controller.ts
@@ -1,34 +1,22 @@
 import { Router, Request, NextFunction, Response } from "express";
 import { validate } from 'class-validator';
 import { OrderService } from "../services/order.service";
-import { isAdmin, isNotLoggedIn } from "../../middleware/auth";
+import { isAdmin, isWebhookRequest } from "../../middleware/auth";
 import { CreateOrderDto, UpdateOrderStatusDto } from "../dtos/order.dto";
 
 const router = Router();
 
-router.post('/', isNotLoggedIn, async (req: Request, res: Response, next: NextFunction) => {
+router.post('/', isWebhookRequest, async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const { birdhouses, price, userId } = req.body;
-    const orderDto = new CreateOrderDto();
-    orderDto.birdhouses = birdhouses;
-    orderDto.price = price;
-    orderDto.userId = userId;
-    const errors = await validate(orderDto);
-    if (errors.length) {
-      return res.status(400).send(errors);
-    }
     const orderService = new OrderService();
-    const createdOrder = await orderService.create(req.body);
-    if (createdOrder === "Not found") return res.status(404).send(createdOrder);
-    if (createdOrder === "Not enough stock") {
-      return res.status(400).send("There is no more stock available for this birdhouse");
-    }
-    res.status(201).send(createdOrder);
+    await orderService.create(req.body);
+    res.sendStatus(201);
   } catch (err) {
     console.log(err.message, err.stack);
     res.sendStatus(500);
   }
 });
+
 router.patch('/status', isAdmin, async (req: Request, res: Response, next: NextFunction) => {
   try {
     const { status, id } = req.body;
